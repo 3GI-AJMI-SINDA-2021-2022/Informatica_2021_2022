a69ce37ec8c4613254e33cce5de111308ba014f4
graziacobello <57944379+graziacobello@users.noreply.github.com>
2023-01-23 09:32:42 +0100

    Update ContatoreGas.java
    tante cose da completare, alcuni errori importanti

--- ContatoreGas/BIN/ContatoreGas.java
+++ ContatoreGas/BIN/ContatoreGas.java
@@ -1,9 +1,10 @@
-Sia data la classe ContatoreGas, sviluppare la classe BollettaCalculator.
+//Sia data la classe ContatoreGas, sviluppare la classe BollettaCalculator.
 Public class ContatoreGas {
-        private double consumoAttuale= 0.0;
-        private double consumoPrecedente= 0.0;
+        private double consumoAttuale;
+        private double consumoPrecedente;
         public ContatoreGas(){
             this.consumoAttuale=0.0;
+            this.consumoPrecedente=0.0;
         }
         public ContatoreGas(double consAtt){
             if(consAtt>=0){
@@ -11,21 +12,30 @@ Public class ContatoreGas {
             }else{
                 this.consumoAttuale= 0.0;
             }
+            this.consumoPrecedente=0.0;
         }
-        public void setRilevazioneAttuale(double Att){
-            if(Att>0 && Att>consumoPrecedente){ 
-            //il consumo attuale deve essere maggiore di quello precedente per poter calcolare la bolletta.
-                this.consumoPrecedente=this.consumoAttuale;
-                this.consumoAttuale= Att;
+        public void setRilevazioneAttuale(double consAtt){
+            if(consAtt>=0){
+                this.rilevazionePrec= this.rilevazioneAtt;
+		if(rilevazioneAtt + consAtt > MAX_CONS){
+			this.rilevazioneAtt = rilevazioneAtt + consAtt - (MAX_CONS);
+		}else{
+			this.rilevazioneAtt=consAtt;
+		}
+	     }	
+
+        }
+        public boolean letturaContatore(double consumo){
+            if(consumo>=0.0){
+                setRilevazioneAttuale(consumo);
+                return true;
             }else{
-                this.consumoAttuale= 0.0;
+                return false;
             }
         }
         public void setRilevazionePrecedente(double consPrec){
             if(consPrec>=0){
                 this.consumoPrecedente= consPrec;
-            }else{
-                this.consumoPrecedente= 0.0;
             }
         }
         public double getConsumoAtt(){
@@ -33,5 +43,8 @@ Public class ContatoreGas {
         }
         public double getConsumoPrec(){
             return consumoPrecedente;
+        }
+        public String toString(){
+                //completare
         }
 }
