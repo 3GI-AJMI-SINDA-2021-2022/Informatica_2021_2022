b50f283f00aed80de371ba40dd9f2c3496b89c71
4GI-AJMI-SINDA-2022-2023 <90970719+4GI-AJMI-SINDA-2022-2023@users.noreply.github.com>
2023-05-21 21:50:07 +0200

    Delete ApplicazioneMVC/BIN directory

--- ApplicazioneMVC/BIN/ApplicazioneTest.java
+++
@@ -1,9 +0,0 @@
-
-import javax.swing.*;
-
-public class ApplicazioneTest{
-    public static void main(String[] args){
-        Applicazione a = new Applicazione("Ajmi Sinda", 1000.00);
-        a.setVisible(true);
-    }
-}
